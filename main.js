aa270a65ffc13ffb5cf0039173c9a87c9e13b777
luzdl <luz-de-leon@outlook.com>
2024-11-13 15:36:48 -0500

    Se elimina archivo menu que no tendrá uso, se agrega función para mostrar botones de menú

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById(botonEmpezar).onclick = function(){
+    document.getElementById("botonFacil").style.display="block";
+    document.getElementById("botonNormal").style.display="block";
+    document.getElementById("botonDificil").style.display="block";
+}
