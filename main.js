52282789c754c6c34e755fe0a96427b01bb160b6
luzdl <luz-de-leon@outlook.com>
2024-11-14 13:55:06 -0500

    se crea función para el manejo de la navegación del menú y el juego

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ document.getElementById(botonEmpezar).onclick = function(){
     document.getElementById("botonNormal").style.display="block";
     document.getElementById("botonDificil").style.display="block";
 }
+//función que carga los scripts correspondientes al nivel de dificultad seleccionado
+function cargarModo(scriptPath) {
+    const script = document.createElement("script");
+    script.src = scriptPath;
+    script.async = true;  
+    document.body.appendChild(script);
+}
+
+document.getElementById("botonFacil").onclick = function() {
+    cargarModo("modoFacil.js");  // Carga el archivo modoFacil.js
+};
+
+document.getElementById("botonNormal").onclick = function() {
+    cargarModo("modoNormal.js"); // Carga el archivo modoNormal.js
+};
+
+document.getElementById("botonDificil").onclick = function() {
+    cargarModo("modoDificil.js"); // Carga el archivo modoDificil.js
+};
